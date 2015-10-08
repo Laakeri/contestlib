7402a7b81f2f01af6b8280ed6e61c89d9a10ec1d
Tuukka Korhonen <tuukka.korhonen@cs.helsinki.fi>
2015-10-08 12:35:12 +0300

    Added treap

---
+++ src/datastructure/treap.cpp
@@ -0,0 +1,80 @@
+// Treap implementation with pointers
+// Expected running time of split and merge is O(log n)
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct node* pnode;
+struct node {
+	pnode l,r;
+	int pr,c;
+	node() {
+		l=0;
+		r=0;
+		c=1;
+		pr=rand();
+	}
+};
+
+// Returns the size of the subtree t
+int cnt(pnode t) {
+	if (t) return t->c;
+	return 0;
+}
+
+// Updates the size of the subtree t
+void upd(pnode t) {
+	if (t) t->c=cnt(t->l)+cnt(t->r)+1;
+}
+
+// Put lazy updates here
+void push(pnode t) {
+	if (t) {
+		// Something
+	}
+}
+
+// Merges trees l and r into tree t
+void merg(pnode& t, pnode l, pnode r) {
+	push(l);
+	push(r);
+	if (!l) t=r;
+	else if(!r) t=l;
+	else {
+		if (l->pr>r->pr) {
+			merg(l->r, l->r, r);
+			t=l;
+		}
+		else {
+			merg(r->l, l, r->l);
+			t=r;
+		}
+	}
+	upd(t);
+}
+
+// Splits tree t into trees l and r
+// Size of tree l will be k
+void split(pnode t, pnode& l, pnode& r, int k) {
+	if (!t) {
+		l=0;
+		r=0;
+		return;
+	}
+	else {
+		push(t);
+		if (cnt(t->l)>=k) {
+			split(t->l, l, t->l, k);
+			r=t;
+		}
+		else {
+			split(t->r, t->r, r, k-cnt(t->l)-1);
+			l=t;
+		}
+	}
+	upd(t);
+}
+
+int main() {
+	
+	
+}
--- src/geom/basic.cpp
+++ src/geom/basic.cpp
@@ -20,13 +20,13 @@ typedef complex<CT> co;
 
 // Return true iff points a, b, c are CCW oriented.
 bool ccw(co a, co b, co c) {
-	return ((c - a) * conj(b - a)).imag() > 0;
+	return ((c - a) * conj(b - a)).Y > 0;
 }
 
 // Return true iff points a, b, c are collinear.
 // NOTE: doesn't make much sense with non-integer CT.
 bool collinear(co a, co b, co c) {
-	return ((c - a) * conj(b - a)).imag() == 0;
+	return ((c - a) * conj(b - a)).Y == 0;
 }
 
 // Rotate x with agle ang
@@ -50,22 +50,22 @@ co interpolate(CT t, co a, co b) {
 //  line defined by a and b.
 //  NOTE: no rounding behavior specified for integers.
 CT projectionParam(co v, co a, co b) {
-	return ((v - a) / (b - a)).real();
+	return ((v - a) / (b - a)).X;
 }
 
 //  Compute the distance of point v from line a..b.
 //  NOTE: Only for non-integers!
 CT pointLineDistance(co p, co a, co b) {
-	return abs(((p - a) / (b - a)).imag()) * abs(b - a);
+	return abs(((p - a) / (b - a)).Y) * abs(b - a);
 }
 
 //  Compute the distance of point v from segment a..b.
 //  NOTE: Only for non-integers!
 CT pointSegmentDistance(co p, co a, co b) {
 	co z = (p - a) / (b - a);
-	if(z.real() < 0) return abs(p - a);
-	if(z.real() > 1) return abs(p - b);
-	return abs(z.imag()) * abs(b - a);
+	if(z.X < 0) return abs(p - a);
+	if(z.X > 1) return abs(p - b);
+	return abs(z.Y) * abs(b - a);
 }
 
 //  Return interpolation parameter between a and b of the point that is also
@@ -74,5 +74,5 @@ CT pointSegmentDistance(co p, co a, co b) {
 CT intersectionParam(co a, co b, co c, co d) {
 	co u = (c - a) / (b - a);
 	co v = (d - a) / (b - a);
-	return (u.real() * v.imag() - u.imag() * v.real()) / (v.imag() - u.imag());
+	return (u.X * v.Y - u.Y * v.X) / (v.Y - u.Y);
 }
