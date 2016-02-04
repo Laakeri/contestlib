7eff6fc5538c285e19483cc283d1c0d9c9de8612
gtrrebel <gtrrebel@gmail.com>
2016-02-04 23:46:59 +0300

    crt edit

--- src/math/crt.cpp
+++ src/math/crt.cpp
@@ -4,35 +4,26 @@
 // Runs in O(log(m)+n) time
 // Overflows only if m overflows
 // Returns {1, {x, m}} if solution exists, and {-1, {0,0}} otherwise
-#include <iostream>
 #include <vector>
-#include <cmath>
 using namespace std;
 typedef long long ll;
 typedef __int128 lll;
 
-ll ee(ll a, ll b, ll ca, ll cb, ll xa, ll xb, ll&x, ll&y) {
-    if (cb==0) {
-        x=xa;
-        if (b==0) y=0;
-        else y=(ca-a*xa)/b;
-        return ca;
-    }
-    else return ee(a, b, cb, ca%cb, xb, xa-(ca/cb)*xb, x, y);
+ll ee(ll ca, ll cb, ll xa, ll xb, ll&x) {
+	if (cb) return ee(cb, ca%cb, xb, xa-(ca/cb)*xb, x);
+	x = xa;
+	return ca;
 }
 
 pair<int, pair<ll, ll>> crt(vector<ll> as, vector<ll> ms) {
-    ll aa = as[0]%ms[0], mm = ms[0], d, a, b, x;
-    for (int i = 1; i < (int) as.size(); i++) {
-        d = __gcd(mm, ms[i]);
-        if ((aa-as[i])%d != 0) return {-1,{0,0}};
-        if (d == ms[i]) continue;
-        a = mm/d;
-        b = ms[i]/d;
-        ee(a, b, a, b, 1, 0, x, b);
-        mm = a*(lll)ms[i];
-        aa = ((lll)aa + ((lll)as[i]-(lll)aa)*(lll)(((lll)a*(lll)(x%mm))%mm))%mm;
-    }
-    if (aa < 0) aa += mm;
-    return {1, {aa, mm}};
+	ll aa = 0, mm = 1, d, a, x;
+	for (int i = 0; i < (int) as.size(); i++) {
+		d = ee(ms[i], mm, 1, 0, x);
+		if ((aa-as[i])%d) return {-1,{0,0}};
+		a = ms[i]/d;
+		mm *= a;
+		aa = (as[i] + (aa-as[i])*(((lll)a*x)%mm))%mm;
+	}
+	if (aa < 0) aa += mm;
+	return {1, {aa, mm}};
 }
