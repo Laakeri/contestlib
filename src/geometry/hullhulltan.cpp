d986c1562b2fb0e7bde5b116f902bcb8da7d97c1
Tuukka Korhonen <tuukka.korhonen@cs.helsinki.fi>
2016-10-18 01:57:45 +0300

    hullhulltan

--- src/geometry/hullhulltan.cpp
+++ src/geometry/hullhulltan.cpp
@@ -1,10 +1,15 @@
 // TCR
+// poinHullTan
 // Finds the common tangents of a convex polygon and a point
 // The polygon should be strictly convex and in counterclockwise order
 // Pointhulltan returns {-1, -1} if the point is inside the polygon, otherwise
 // it returs {maximal, minimal} vertices in terms of visibility from point p
 // Remember to implement the special case n<=2
 // Points on the boundary are considered to be inside
+// hullHullTan
+// Finds the common tangents of two convex polygons
+// All of the conditions as above and it probably does not work if n<=2 or m<=2
+// 1 is maximal and -1 is minimal
 #include <bits/stdc++.h>
 #define F first
 #define S second
@@ -21,9 +26,7 @@ bool up(co p, vector<co>& h, int a, int b, int d){
 	return (ll)d*ccw(p, h[(a+n)%n], h[(b+n)%n])<=0;
 }
 int getTanP(co p, vector<co>& h, int d) {
-	int n=h.size();
-	int mi=0;
-	int ma=n;
+	int n=h.size();int mi=0;int ma=n;
 	while (mi+1<ma){
 		int mid=(mi+ma)/2;
 		if (up(p, h, mi, mi+1, d)){
@@ -60,4 +63,50 @@ pair<int, int> pointHullTan(co p, vector<co>& h) {
 	int t1=getTanP(p, h, -1);
 	if (t1==-1) return {-1, -1};
 	return {getTanP(p, h, 1), t1};
+}
+bool up2(vector<co>& h1, vector<co>& h, int a, int b, int d1, int d2){
+	int n=h.size();
+	int k=getTanP(h[(b+n)%n], h1, d1);
+	return (ll)d2*ccw(h[(a+n)%n], h[(b+n)%n], h1[k])<=0;
+}
+pair<int, int> getTanH(vector<co>& h1, vector<co>& h, int d1, int d2){
+	int n=h.size();int mi=0;int ma=n;
+	while (mi+1<ma){
+		int mid=(mi+ma)/2;
+		if (up2(h1, h, mi, mi+1, d1, d2)){
+			if (up2(h1, h, mid+1, mid, d1, d2)) ma=mid;
+			else if(up2(h1, h, mi, mid, d1, d2)) mi=mid;
+			else ma=mid;
+		}
+		else{
+			if (up2(h1, h, mid, mid+1, d1, d2)) mi=mid;
+			else if(up2(h1, h, mid, mi, d1, d2)) mi=mid;
+			else ma=mid;
+		}
+	}
+	int step=0;
+	if (d2==1){
+		ma%=n;
+		while (up2(h1, h, ma, ma+1, d1, d2)){
+			ma=(ma+1)%n;step++;
+			assert(step<2);
+		}
+		return {getTanP(h[ma], h1, d1), ma};
+	}
+	else{
+		while (up2(h1, h, mi, mi+1, d1, d2)){
+			mi=(mi+1)%n;step++;
+			assert(step<3);
+		}
+		if (up2(h1, h, mi, mi-1, d1, d2)) mi=(mi-1+n)%n;
+		return {getTanP(h[mi], h1, d1), mi};
+	}
+}
+vector<pair<int, int> > hullHullTan(vector<co>& h1, vector<co>& h2){
+	vector<pair<int, int> > ret;
+	ret.push_back(getTanH(h1, h2, 1, 1));
+	ret.push_back(getTanH(h1, h2, 1, -1));
+	ret.push_back(getTanH(h1, h2, -1, 1));
+	ret.push_back(getTanH(h1, h2, -1, -1));
+	return ret;
 }
